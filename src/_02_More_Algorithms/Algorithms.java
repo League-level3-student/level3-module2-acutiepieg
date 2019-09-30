a5ac02b1ffb19b61dbf985995a7be16c5ab82b81
League Student <league@iMac-21.attlocal.net>
2019-09-30 16:56:42 -0700

    started SearchingandSortingAlgorithms

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,34 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i) == "cracked") {
+				return i;
+			}
+		}
+
+		return -1;
 	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		int count  = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if(oysters.get(i) == true) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static Double findTallest(List<Double> peeps) {
+		int tallestIndex = 0;
+		for(int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i) > peeps.get(tallestIndex)) {
+				tallestIndex = i;
+			}
+		}
+		
+		return peeps.get(tallestIndex);
+	}
+
 }
